ade4f478a23d30f1ff2bcfde81e6e52ee5e93124
Karol Zając <karolzajac.0407@gmail.com>
2023-11-26 11:19:01 +0100

    2048 logic works

--- src/lib/the2k48/The2k48Grid.svelte
+++ src/lib/the2k48/The2k48Grid.svelte
@@ -1,128 +1,175 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { onMount, tick } from 'svelte';
 	import The2k48Field from '$lib/the2k48/The2k48Field.svelte';
 
-	const fieldsNumber = 3;
-	let fields: [number, number][] = createFields(fieldsNumber);
+	type Position = [number, number];
+	type Arrow = 'ArrowUp' | 'ArrowDown' | 'ArrowLeft' | 'ArrowRight';
 
-	let gameMap: Map<[number, number], number> = new Map(fields.map((field) => [field, 0]));
-	let tiles: Map<string, [number, number]> = new Map();
-
-	function createFields(num: number): [number, number][] {
-		let fields = Array<[number, number]>();
-		for (let i = 0; i < num; i++) {
-			for (let j = 0; j < num; j++) {
-				fields.push([i, j]);
-			}
-		}
-		return fields;
-	}
+	const fieldsNumber = 4;
+	let fields: [number, number][] = Array.from({ length: fieldsNumber }, (_, i) =>
+		Array.from({ length: fieldsNumber }, (_, j): Position => [i, j])
+	).flat();
+	let gameMap: Map<Position, number> = new Map(fields.map((field) => [field, 0]));
+	let tiles: Map<string, Position> = new Map();
 
 	function spawn() {
 		let emptyFields = fields.filter((field) => gameMap.get(field) === 0);
 		if (emptyFields.length === 0) return;
 		let randomField = emptyFields[Math.floor(Math.random() * emptyFields.length)];
-		gameMap.set(randomField, 2);
+		gameMap.set(randomField, Math.random() < 0.9 ? 2 : 4);
 		gameMap = new Map(gameMap.entries());
 		tiles.set(Date.now().toString(), randomField);
 		tiles = new Map(tiles.entries());
 	}
-	function getKey(newPos: [number, number]): [number, number] {
+	function getKey(newPos: Position): Position {
 		for (let key of gameMap.keys()) {
 			if (key[0] == newPos[0] && key[1] == newPos[1]) return key;
 		}
 		return newPos;
 	}
 
-	function move(tile: [string, [number, number]], arg: 'left' | 'right' | 'up' | 'down') {
-		let shifter: (arg: [number, number]) => [number, number];
+	function isWithinBounds(position: Position): boolean {
+		return (
+			position[0] >= 0 &&
+			position[0] < fieldsNumber &&
+			position[1] >= 0 &&
+			position[1] < fieldsNumber
+		);
+	}
+	function move(
+		tile: [string, Position],
+		arg: Arrow,
+		transformed: Array<string>
+	): [boolean, Array<string>] {
+		let shifter: (arg: Position) => Position;
 		switch (arg) {
-			case 'left':
+			case 'ArrowLeft':
 				shifter = ([x, y]) => [x - 1, y];
 				break;
-			case 'right':
+			case 'ArrowRight':
 				shifter = ([x, y]) => [x + 1, y];
 				break;
-			case 'up':
+			case 'ArrowUp':
 				shifter = ([x, y]) => [x, y - 1];
 				break;
-			case 'down':
+			case 'ArrowDown':
 				shifter = ([x, y]) => [x, y + 1];
 				break;
 		}
 		let [id, position] = tile;
-
 		const privGameMap = new Map(gameMap.entries());
 		const privTiles = new Map(tiles.entries());
-		position = getKey(position);
-		let newPos: [number, number];
-		while (
-			shifter(position)[0] >= 0 &&
-			shifter(position)[0] < fieldsNumber &&
-			shifter(position)[1] >= 0 &&
-			shifter(position)[1] < fieldsNumber
-		) {
-			newPos = getKey(shifter(position));
 
-			if (privGameMap.get(newPos) == privGameMap.get(position)) {
-				console.log('ping');
-				for (let [key, value] of privTiles.entries()) {
-					if (value == newPos) {
+		function processTile(position: Position, newPos: Position): string | boolean {
+			if (privGameMap.get(newPos) === 0) {
+				privTiles.set(id, newPos);
+				privGameMap.set(newPos, privGameMap.get(position)!);
+				privGameMap.set(position, 0);
+				for (const [key, positionPriv] of privTiles.entries()) {
+					if (privGameMap.get(getKey(positionPriv)) == 0) privTiles.delete(key);
+				}
+				return true;
+			} else if (privGameMap.get(newPos) === privGameMap.get(position)) {
+				for (const [key, positionPriv] of privTiles.entries()) {
+					if (positionPriv == newPos) {
+						if (transformed.includes(key)) {
+							return false;
+						}
 						privTiles.delete(key);
 						break;
 					}
 				}
+
 				privGameMap.set(newPos, privGameMap.get(newPos)! * 2);
 				privGameMap.set(position, 0);
 				privTiles.set(id, newPos);
-				gameMap = new Map(privGameMap.entries());
+				for (const [key, positionPriv] of privTiles.entries()) {
+					if (privGameMap.get(getKey(positionPriv)) == 0) privTiles.delete(key);
+				}
+				return id;
+			}
 
-				tiles = new Map(privTiles.entries());
-				return;
-			} else if (privGameMap.get(newPos) == 0) {
-				privTiles.set(id, newPos);
-				privGameMap.set(newPos, privGameMap.get(position)!);
-				privGameMap.set(position, 0);
+			return false;
+		}
+
+		position = getKey(position);
+		let newPos: Position;
+		let changeFlag = false;
+
+		while (isWithinBounds(shifter(position))) {
+			newPos = getKey(shifter(position));
+			const result = processTile(position, newPos);
+			if (result === true) {
+				changeFlag = true;
 				position = newPos;
-			} else {
-				gameMap = new Map(privGameMap.entries());
+			} else if (result === id) {
+				transformed.push(id);
+				changeFlag = true;
+				break;
+			} else break;
+		}
+		gameMap = new Map(privGameMap.entries());
+		tiles = new Map(privTiles.entries());
+		return [changeFlag, [...transformed]];
+	}
 
-				tiles = new Map(privTiles.entries());
-				return;
+	function iterator(type: Arrow): IterableIterator<[string, Position]> {
+		let predicate: (value1: Position, value2: Position) => number;
+		switch (type) {
+			case 'ArrowUp':
+				predicate = (value1, value2) => value1[1] - value2[1];
+				break;
+			case 'ArrowDown':
+				predicate = (value1, value2) => value2[1] - value1[1];
+				break;
+			case 'ArrowLeft':
+				predicate = (value1, value2) => value1[0] - value2[0];
+				break;
+			case 'ArrowRight':
+				predicate = (value1, value2) => value2[0] - value1[0];
+				break;
+		}
+		return new Map(
+			[...tiles].sort(([key1, value1], [key2, value2]) => predicate(value1, value2))
+		).entries();
+	}
+
+	function iterateTiles(type: Arrow):boolean {
+		let transformed: Array<string> = new Array();
+		let changeFlag = false;
+		for (let [id, position] of iterator(type)) {
+			const [flag, newTransformed] = move([id, position], type, transformed);
+			if (flag) {
+				changeFlag = true;
+				transformed = newTransformed;
 			}
 		}
-
-		gameMap = new Map(privGameMap.entries());
-
-		tiles = new Map(privTiles.entries());
+		return changeFlag;
 	}
-	function onKeyDown(e: any) {
-		const { keyCode } = e;
-		switch (keyCode) {
-			case 38:
-				tiles.forEach((tile, value) => {
-					move([value, tile], 'up');
-				});
+
+	function onKeyDown(e: KeyboardEvent):void {
+		const { key } = e;
+		let changeFlag = false;
+		switch (key) {
+			case 'ArrowUp':
+				changeFlag = iterateTiles('ArrowUp');
 				break;
-			case 40:
-				tiles.forEach((tile, value) => {
-					move([value, tile], 'down');
-				});
+			case 'ArrowDown':
+				changeFlag = iterateTiles('ArrowDown');
 				break;
-			case 37:
-				tiles.forEach((tile, value) => {
-					move([value, tile], 'left');
-				});
+			case 'ArrowLeft':
+				changeFlag = iterateTiles('ArrowLeft');
 				break;
-			case 39:
-				tiles.forEach((tile, value) => {
-					move([value, tile], 'right');
-				});
+			case 'ArrowRight':
+				changeFlag = iterateTiles('ArrowRight');
 				break;
 		}
-		spawn();
+		if (changeFlag) spawn();
 	}
-	onMount(() => {
+
+	onMount(async() => {
+		spawn();
+		await tick();
 		spawn();
 	});
 </script>
@@ -130,11 +177,6 @@
 <svelte:window on:keydown={onKeyDown} />
 
 <div class="relative w-[400px] h-[400px] bg-red-200">
-	<!-- {#each tiles as [id, position] (id)}
-		{#if gameMap.get(position) != 0}
-			<The2k48Field {position} value={gameMap.get(position)} {fieldsNumber} />
-		{/if}
-	{/each} -->
 	{#each tiles as [id, position] (id)}
 		<The2k48Field {position} value={gameMap.get(position)} {fieldsNumber} />
 	{/each}
@@ -142,10 +184,4 @@
 {#each tiles as [id, position], index (id)}
 	<div>{index} essa {position} val{gameMap.get(position)}</div>
 	<div></div>
-{/each}
-nowe
-{#each gameMap as [position, value], index}
-	{#if value != 0}
-		<div>{index}-{value} {position}</div>
-	{/if}
 {/each}
